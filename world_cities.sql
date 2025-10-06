41717f7b6a043b58c6ef50644f31010269eddce8
FastDevelopmentP <pete5fast@gmail.com>
2025-10-05 22:22:34 -0400

    First Commit

--- world_cities.sql
+++ world_cities.sql
@@ -1,3 +1,14 @@
 -- Write your code below this line
+DROP TABLE IF EXISTS cities;
+-- Create the cities table
+CREATE TABLE cities (
+    id INTEGER PRIMARY KEY,
+    name TEXT,
+    country TEXT,
+    population INTEGER
+);
+
+INSERT INTO cities (id, name, country, population) VALUES
+
 -- You can run this file with the command sqlite3 world_cities.db < world_cities.sql
 -- If you don't run this file before running the tests, some tests will fail even though your queries may be correct!
